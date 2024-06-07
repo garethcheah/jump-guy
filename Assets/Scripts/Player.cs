f615036318c01a7633621e80290e1b540349d035
garethcheah <garethcheah@gmail.com>
2024-06-07 01:08:24 -0700

    Initial attempt to implement jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private static string runConditionName = "isRunning";
+    private static string groundedConditionName = "isGrounded";
+    private bool isGrounded = true;
+    private float horizontalInput;
     private Animator playerAnimator;
     private SpriteRenderer playerSR;
+    private Rigidbody2D playerRB;
 
     [SerializeField]
     private float runSpeed = 5f;
+
+    [SerializeField]
+    private float jumpPower = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
         playerSR = GetComponent<SpriteRenderer>();
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         MovePlayer();
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            JumpPlayer();
+        }
     }
 
     private void MovePlayer()
     {
-        // Gets input from axis
-        float input = Input.GetAxis("Horizontal");
-
-        // Calculate movement
-        Vector2 movement = new Vector2(input * Time.deltaTime * runSpeed, 0f);
-
-        // Applies movement
-        transform.Translate(movement);
+        // Apply player movement
+        playerRB.velocity = new Vector2(horizontalInput * runSpeed, playerRB.velocity.y);
 
         // Initiate running animation if key down is detected
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
@@ -47,5 +57,18 @@ public class Player : MonoBehaviour
         {
             playerAnimator.SetBool(runConditionName, false);
         }
+    }
+
+    private void JumpPlayer()
+    {
+        playerRB.velocity = new Vector2(playerRB.velocity.x * runSpeed, jumpPower);
+        isGrounded = false;
+        playerAnimator.SetBool(groundedConditionName, isGrounded);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        playerAnimator.SetBool(groundedConditionName, isGrounded);
     }
 }
